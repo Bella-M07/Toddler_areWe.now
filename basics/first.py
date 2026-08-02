5277cbb60e96292470685d408761886b34f9747e
Bella-M07 <dostoevskyf.1101@gmail.com>
2026-08-03 01:26:18 +0530

    well, few things comments, escape & print

---
+++ basics/first.py
@@ -0,0 +1,30 @@
+print("Hello, world!")
+# print(5)
+
+"""
+yoooooooooooooooooooooo!
+"""
+
+print("For the tainted sorrow...") #title
+
+print("""For the tainted sorrow:
+Today too snow falls on it;
+For the tainted sorrow:
+Today too wind blows on it.
+
+For the tainted sorrow
+Is like, say, a fox's fur;
+For the tainted sorrow,
+Covered with snow, curls up.
+
+For the tainted sorrow
+Has no desires or wishes;
+For the tainted sorrow
+In its torpor dreams of death.
+
+For the tainted sorrow
+Frightens me piteously;
+For the tainted sorrow
+Can't be remedied, and the sun sets...""")
+
+print("\t\t\t\t\t~Chuuya Nakhahara")
--- basics/hello.py
+++ basics/hello.py
@@ -1,1 +1,4 @@
 print("Hello, world!\nJuly 30th, 2026. \nFinally.")
+print("Hey", 6, 7, sep="--", end="\nYooo! ")
+print("well, who is it?")
+# so, we got file=file for write too and flush=flsuh
